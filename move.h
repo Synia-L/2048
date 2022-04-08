d2d659624125cc86c155184025ad74bd17725d3d
Synia-L <40327117+Synia-L@users.noreply.github.com>
2022-04-08 15:16:27 +0800

    Update move.h

--- move.h
+++ move.h
@@ -1,26 +1,3 @@
-/*************************************************************************
- * File    : move.h
- * Author  : Quan Zhuo <zhuo.quan@outlook.com>
- * Purpose : contains function and macro declaration
- * Created : Sun 06 Nov 2016 05:55:20 PM CST
- *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write to the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
- *
- *************************************************************************
- */
-
 #include <stdbool.h>
 
 #include <stdio.h>
